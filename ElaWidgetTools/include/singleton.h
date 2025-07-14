96fb00c2681f82a3a5877b8c124d95236585a3b7
chenjunhao <120310774+Spectre-2022@users.noreply.github.com>
2025-07-14 15:41:31 +0800

    更新CMake配置中的QT_SDK_DIR路径，修正为正确的Qt SDK目录；更新README文件，添加Beta-V0.0.0.2.5版本信息及更新说明。

--- ElaWidgetTools/include/singleton.h
+++ ElaWidgetTools/include/singleton.h
@@ -3,61 +3,57 @@
 
 #include <QMutex>
 
-namespace ElaWidgetTools {
-
-template <typename T>
-class Singleton
+namespace ElaWidgetTools
 {
-public:
+
+template <typename T> class Singleton
+{
+  public:
     static T* getInstance();
 
-private:
+  private:
     Q_DISABLE_COPY(Singleton)
 };
 
-template <typename T>
-T* Singleton<T>::getInstance()
+template <typename T> T* Singleton<T>::getInstance()
 {
     static QMutex mutex;
     QMutexLocker locker(&mutex);
     static T* instance = nullptr;
-    if (instance == nullptr)
-    {
+    if (instance == nullptr) {
         instance = new T();
     }
     return instance;
 }
 
-#define Q_SINGLETON_CREATE(Class)               \
-private:                                        \
-    friend class Singleton<Class>;              \
-                                                \
-public:                                         \
-    static Class* getInstance()                 \
-    {                                           \
-        return Singleton<Class>::getInstance(); \
-    }
+#define Q_SINGLETON_CREATE(Class)                                                                                                                              \
+  private:                                                                                                                                                     \
+    friend class Singleton<Class>;                                                                                                                             \
+                                                                                                                                                               \
+  public:                                                                                                                                                      \
+    static Class* getInstance() { return Singleton<Class>::getInstance(); }
 
-#define Q_SINGLETON_CREATE_H(Class) \
-private:                            \
-    static Class* _instance;        \
-                                    \
-public:                             \
+#define Q_SINGLETON_CREATE_H(Class)                                                                                                                            \
+  private:                                                                                                                                                     \
+    static Class* _instance;                                                                                                                                   \
+                                                                                                                                                               \
+  public:                                                                                                                                                      \
     static Class* getInstance();
 
-#define Q_SINGLETON_CREATE_CPP(Class)  \
-    Class* Class::_instance = nullptr; \
-    Class* Class::getInstance()        \
-    {                                  \
-        static QMutex mutex;           \
-        QMutexLocker locker(&mutex);   \
-        if (_instance == nullptr)      \
-        {                              \
-            _instance = new Class();   \
-        }                              \
-        return _instance;              \
+#define Q_SINGLETON_CREATE_CPP(Class)                                                                                                                          \
+    Class* Class::_instance = nullptr;                                                                                                                         \
+    Class* Class::getInstance()                                                                                                                                \
+    {                                                                                                                                                          \
+        static QMutex mutex;                                                                                                                                   \
+        QMutexLocker locker(&mutex);                                                                                                                           \
+        if (_instance == nullptr) {                                                                                                                            \
+            _instance = new Class();                                                                                                                           \
+        }                                                                                                                                                      \
+        return _instance;                                                                                                                                      \
     }
 
-}
+}  // namespace ElaWidgetTools
 
-#endif // SINGLETON_H
+using ElaWidgetTools::Singleton;
+
+#endif  // SINGLETON_H
